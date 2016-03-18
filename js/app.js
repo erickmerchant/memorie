ceb1e2e779303832688fd30658d55dec6cca4332
Erick Merchant <erickmerchant@gmail.com>
2016-03-18 00:08:22 -0700

    Saving work. Broken

--- js/app.js
+++ js/app.js
@@ -1,7 +1,156 @@
-var framework = require('./framework')('main')
+var fetch = require('./fetch')
+var framework = require('./framework')
+var tag = framework.tag
+var redirect = framework.redirect
+var route = framework.route
+var run = framework.run
 
-framework.redirect('/', '/clicccck/123')
+redirect('/', '/tasks')
 
-framework.route('/clicccck/:id', function (params) {
-  console.log(params)
+route('/tasks', function (params, app) {
+  fetch('/api/tasks')
+  .then(function (tasks) {
+    app.render(viewList({tasks}))
+  })
+  .catch(function (error) {
+    app.render(viewList({error}))
+  })
 })
+
+route('/tasks/new', function (params, app) {
+  fetch('/api/tasks')
+  .then(function (tasks) {
+    app.render(viewList({tasks}))
+  })
+  .catch(function (error) {
+    app.render(viewList({error}))
+  })
+})
+
+route('/tasks/:id', function (params, app) {
+  fetch(['/api/tasks/' + params.id, '/api/tasks'])
+  .then(function ([task, tasks]) {
+    app.render(viewItem({task, tasks}, app))
+  })
+  .catch(function (error) {
+    app.render(viewItem({error}, app))
+  })
+})
+
+run('main')
+
+function viewList (state) {
+  return tag`<div>
+    <div class="clearfix">
+      ${viewError(state)}
+      ${viewItems(state)}
+    </div>
+  </div>`
+}
+
+function viewItem (state, app) {
+  return tag`<div>
+    <div class="clearfix">
+      ${viewItems(state)}
+    </div>
+    <form class="fixed top-0 right-0 bottom-0 left-0 bg-white p2 mt3" onsubmit=${state.task ? saveItem(state.task.id, app) : createItem(app)}>
+      ${viewError(state)}
+      <h1>${state.task ? state.task.title : 'New Task'}</h1>
+      <label class="block my2">
+        Title
+        <input class="col-12 sm-col-12 p1 input" type="text" placeholder="" name="title" value="${state.task ? state.task.title : ''}">
+      </label>
+      <label class="block my2">
+        Content
+        <textarea class="col-12 sm-col-12 p1 textarea" type="text" placeholder="" name="content">${state.task ? state.task.content : ''}</textarea>
+      </label>
+      <label class="block my2">
+        Closed
+        <input class="field" type="checkbox" name="closed" checked="${state.task && state.task.closed ? 'checked' : ''}">
+      </label>
+      <div class="inline-block mr1 mb1"><button class="btn btn-primary btn-big bg-fuchsia" type="submit">Save</button></div>
+      ${state.task ? tag`<div class="inline-block mr1 mb1"><button class="btn btn-primary bg-purple" type="button" onclick=${deleteItem(state.task.id, app)}>Delete</button></div>` : ''}
+    </form>
+  </div>`
+}
+
+function viewError (state) {
+  if (!state.error) return ''
+
+  return tag`
+  <div class="mt2 mb0 col col-12">
+    <div class="p2 bg-purple white">${state.error.message}</div>
+  </div>`
+}
+
+function viewItems (state) {
+  return tag`<div class="col col-12">
+    <h1>Tasks</h1>
+    <ul class="fuchsia list-reset">
+      ${(state.tasks || []).map((item) => {
+        return tag`<li class="p1">
+          ${item.closed ? tag`<del><a class="fuchsia" href="/tasks/${item.id}">${item.title || 'untitled'}</a></del>` : tag`<a class="fuchsia" href="/tasks/${item.id}">${item.title || 'untitled'}</a>`}
+        </li>`
+      })}
+    </ul>
+    <a class="btn btn-primary btn-big bg-fuchsia" href="/tasks/new">Add New</a>
+  </div>`
+}
+
+function createItem (app) {
+  return function (e) {
+    e.preventDefault()
+
+    fetch('/api/tasks', {
+      method: 'post',
+      body: JSON.stringify({
+        title: this.title.value,
+        content: this.content.value,
+        closed: this.closed.checked
+      })
+    })
+    .then(function () {
+      app.redirect('/tasks')
+    })
+    .catch(function (error) {
+      app.render(viewItem({error}, app))
+    })
+  }
+}
+
+function saveItem (id, app) {
+  return function (e) {
+    e.preventDefault()
+
+    fetch('/api/tasks/' + id, {
+      method: 'put',
+      body: JSON.stringify({
+        title: this.title.value,
+        content: this.content.value,
+        closed: this.closed.checked
+      })
+    })
+    .then(function () {
+      app.redirect('/tasks')
+    })
+    .catch(function (error) {
+      app.render(viewItem({error}, app))
+    })
+  }
+}
+
+function deleteItem (id, app) {
+  return function (e) {
+    e.preventDefault()
+
+    fetch('/api/tasks/' + id, {
+      method: 'delete'
+    })
+    .then(function () {
+      app.redirect('/tasks')
+    })
+    .catch(function (error) {
+      app.render(viewItem({error}, app))
+    })
+  }
+}
--- js/framework.js
+++ js/framework.js
@@ -1,50 +1,78 @@
 var vdom = require('virtual-dom')
+var VText = require('virtual-dom/vnode/vtext')
 var hyperx = require('hyperx')
 var mainLoop = require('main-loop')
 var singlePage = require('single-page')
 var trieRoute = require('trie-route')
 var catchLinks = require('catch-links')
-var hx = hyperx(vdom.h)
-var initialState
-var loop = mainLoop(initialState, function (state) {
-  return hx`<a href="/click-here">Click Here</a>`
-}, {
+var tag = hyperx(vdom.h)
+var loopOptions = {
   document,
   create: require('virtual-dom/create-element'),
   diff: require('virtual-dom/diff'),
   patch: require('virtual-dom/patch')
-})
+}
+var loop = mainLoop(null, function (dom) {
+  if (dom === null) {
+    return new VText('')
+  }
+
+  return dom
+}, loopOptions)
 var router = trieRoute.create()
+var current
+var page
 
-module.exports = function (selector) {
-  var current
-  var page = singlePage(function (href) {
-    current = href
+module.exports = {
+  tag,
+  route,
+  redirect,
+  run
+}
 
+function route (from, callback) {
+  router.path(from, function (params) {
+    var id = Symbol()
+
+    current = id
+
+    callback(params, {
+      render: function (dom) {
+        if (id === current) {
+          loop.update(dom)
+        }
+      },
+      redirect: function (to) {
+        setTimeout(function () {
+          page(to)
+        }, 0)
+      }
+    })
+  })
+}
+
+function redirect (from, to) {
+  router.path(from, function () {
+    setTimeout(function () {
+      page(to)
+    }, 0)
+  })
+}
+
+function run (selector) {
+  page = singlePage(function (href) {
     var error = router.process(href)
-    console.log('error:', error)
+
+    if (error) {
+      console.error(error)
+    }
   })
 
-  document.querySelector(selector).appendChild(loop.target)
-
   catchLinks(window, function (href) {
-    console.log('dsfsdf')
+    console.log(href)
 
     page(href)
   })
 
-  return {
-    route: router.path,
-    redirect: function (from, to) {
-      setTimeout(function () {
-        router.path(from, function () {
-          page.push(to)
-        })
-
-        console.log(current)
-
-        router.process(current)
-      }, 0)
-    }
-  }
+  document.querySelector(selector).appendChild(loop.target)
 }
