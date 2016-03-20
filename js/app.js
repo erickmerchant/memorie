3436e95958014594ca1449f1712707f5173993ab
Erick Merchant <erickmerchant@gmail.com>
2016-03-20 01:06:02 -0700

    Final product

--- js/app.js
+++ js/app.js
@@ -21,6 +21,8 @@ route('/tasks/new', function (params, app) {
   fetch('/api/tasks')
   .then(function (tasks) {
     app.render(view({mode: 'create', tasks}, app))
+
+    setTimeout(() => document.querySelector('form').scrollIntoView(), 10)
   })
   .catch(function (error) {
     app.render(view({error}, app))
@@ -31,6 +33,8 @@ route('/tasks/:id', function (params, app) {
   fetch(['/api/tasks/' + params.id, '/api/tasks'])
   .then(function ([task, tasks]) {
     app.render(view({mode: 'edit', task, tasks}, app))
+
+    setTimeout(() => document.querySelector('form').scrollIntoView(), 10)
   })
   .catch(function (error) {
     app.render(view({error}, app))
@@ -40,7 +44,15 @@ route('/tasks/:id', function (params, app) {
 run('main')
 
 function view (state, app) {
-  return tag`<div class="mt4 p2">
+  return tag`<div>
+    <div class="clearfix white bg-maroon p2 bold">
+      <div class="left">
+        <span class="btn">Memorie</span>
+      </div>
+      <div class="right">
+        <a class="btn border--white white rounded" href="/tasks/new">+ Add</a>
+      </div>
+    </div>
     ${state.error ? tag`<div class="mt2 mb0 col col-12">
       <div class="p2 bg-purple white">${state.error.message}</div>
     </div>` : ''}
@@ -50,37 +62,31 @@ function view (state, app) {
         return viewItem(state.task, app)
       }
 
-      return tag`<div class="max-width-4 mx-auto">
+      return tag`<div class="max-width-4 mx-auto p1">
         ${item.closed ? tag`<del><a class="fuchsia" href="/tasks/${item.id}">${item.title || 'untitled'}</a></del>` : tag`<a class="fuchsia" href="/tasks/${item.id}">${item.title || 'untitled'}</a>`}
       </div>`
     })}
-    <div class="fixed top-0 right-0 left-0 white bg-maroon p2 bold">
-      <div class="left">
-        <span class="btn">Memorie</span>
-      </div>
-      <div class="right">
-        <a class="btn border--white white rounded" href="/tasks/new">+ Add</a>
-      </div>
-    </div>
   </div>`
 }
 
 function viewItem (task, app) {
-  return tag`<form class="black pb2 max-width-4 mx-auto" onsubmit=${task ? saveItem(task.id, app) : createItem(app)}>
-    <label class="block my2">
-      Title
-      <input class="p1 input" type="text" placeholder="" name="title" value="${task ? task.title : ''}">
-    </label>
-    <label class="block my2">
-      Content
-      <textarea class="p1 textarea" type="text" placeholder="" name="content">${task ? task.content : ''}</textarea>
-    </label>
-    <label class="block my2">
-      Closed
-      <input class="field" type="checkbox" name="closed" checked="${task && task.closed ? 'checked' : ''}">
-    </label>
-    <div class="inline-block mr1 mb1"><button class="btn btn-primary btn-big bg-fuchsia" type="submit">Save</button></div>
-    ${task ? tag`<div class="inline-block mr1 mb1"><button class="btn btn-primary bg-purple" type="button" onclick=${deleteItem(task.id, app)}>Delete</button></div>` : ''}
+  return tag`<form class="bg-silver py3" onsubmit=${task ? saveItem(task.id, app) : createItem(app)}>
+    <div class="black pb2 max-width-4 mx-auto">
+      <label class="block my2">
+        Title
+        <input class="p1 input" type="text" placeholder="" name="title" value="${task ? task.title : ''}">
+      </label>
+      <label class="block my2">
+        Content
+        <textarea class="p1 textarea" type="text" placeholder="" name="content">${task ? task.content : ''}</textarea>
+      </label>
+      <label class="block my2">
+        Closed
+        <input class="field" type="checkbox" name="closed" checked="${task && task.closed ? 'checked' : ''}">
+      </label>
+      <div class="inline-block mr1 mb1"><button class="btn btn-primary btn-big bg-fuchsia" type="submit">Save</button></div>
+      ${task ? tag`<div class="inline-block mr1 mb1"><button class="btn btn-primary bg-purple" type="button" onclick=${deleteItem(task.id, app)}>Delete</button></div>` : ''}
+    </div>
   </form>`
 }
 
@@ -100,7 +106,7 @@ function createItem (app) {
       app.redirect('/tasks')
     })
     .catch(function (error) {
-      app.render(viewItem({error}, app))
+      app.render(view({error}, app))
     })
   }
 }
@@ -121,7 +127,7 @@ function saveItem (id, app) {
       app.redirect('/tasks')
     })
     .catch(function (error) {
-      app.render(viewItem({error}, app))
+      app.render(view({error}, app))
     })
   }
 }
@@ -137,7 +143,7 @@ function deleteItem (id, app) {
       app.redirect('/tasks')
     })
     .catch(function (error) {
-      app.render(viewItem({error}, app))
+      app.render(view({error}, app))
     })
   }
 }
