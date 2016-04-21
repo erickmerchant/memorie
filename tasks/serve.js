12a6e4f0e9ae3aff100b1109131093013d66321e
Erick Merchant <erickmerchant@gmail.com>
2016-04-20 20:10:31 -0700

    Start postgres too

--- tasks/serve.js
+++ tasks/serve.js
@@ -2,9 +2,20 @@
 
 var nodemon = require('nodemon')
 var environment = require('dotenv').config()
+var spawn = require('child_process').spawn
 
 module.exports = function serve () {
   return new Promise(function (resolve, reject) {
+    const postgres = spawn('postgres', ['-D', '/usr/local/var/postgres'])
+
+    postgres.stdout.on('data', function (data) {
+      console.log(`${data}`)
+    })
+
+    postgres.stderr.on('data', function (data) {
+      console.error(`${data}`)
+    })
+
     nodemon({
       ext: 'js,html',
       ignore: [ './compiled/' ],
