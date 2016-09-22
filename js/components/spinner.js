e0aa7d427e4317c363861bd869f454492a6514ae
Erick Merchant <erickmerchant@gmail.com>
2016-09-21 22:35:23 -0700

    Some css is clearer with inheritence and inline styles

--- js/components/spinner.js
+++ js/components/spinner.js
@@ -4,5 +4,5 @@ const html = diff.html
 module.exports = function (app, size) {
   const borderWidth = Math.ceil(size / 4)
 
-  return html`<div class="circle rotate-500ms-linear-infinite border white" style="height: ${size}px; width: ${size}px; border-width: ${borderWidth}px; border-bottom-color: transparent; border-left-color: transparent;"></div>`
+  return html`<div class="spinner" style="height: ${size}px; width: ${size}px; border-width: ${borderWidth}px"></div>`
 }
