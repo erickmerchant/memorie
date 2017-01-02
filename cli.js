0dcdef75cf3d63ec77e87e59a4b431d687c9c94a
Erick Merchant <erickmerchant@gmail.com>
2017-01-01 18:44:22 -0800

    Simplify cli

--- cli.js
+++ cli.js
@@ -1,20 +1,13 @@
 'use strict'
 
-const sergeant = require('sergeant')
-const app = sergeant().describe('Build CLI for Memorie')
+const sergeant = require('sergeant/command')
+const app = sergeant()
 const css = require('./tasks/css')
 const js = require('./tasks/js')
 const svg = require('./tasks/svg')
 const serve = require('./tasks/serve')
 
-app.command('update')
-.describe('Build the site once')
-.action(function () {
-  return Promise.all([svg(), css(), js()])
-})
-
-app.command('watch')
-.describe('Build the site then watch for changes. Run a server')
+app.describe('Build the site then watch for changes. Run a server')
 .action(function () {
   return Promise.all([
     svg(),
